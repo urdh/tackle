87e7d07f9817c0e715e2dd03d85ae243dacd4e8c
Simon Sigurdhsson <simon.sigurdhsson@evidente.se>
2019-04-24 11:29:18 +0200

    urdh theme: Update git prompt to match newer fish releases

--- themes/urdh/fish_prompt.fish
+++ themes/urdh/fish_prompt.fish
@@ -13,15 +13,34 @@ function _urdh_theme_cwd --description 'Print current working directory'
 end
 
 function _urdh_theme_vcs --description 'Write out the VCS prompt'
+    # Shared settings
+    _ifnotset _urdh_theme_vcs_color_branch 'black' '--bold'
+    _ifnotset _urdh_theme_vcs_color_merging 'red' '--bold'
+
+    # Git prompt settings
+    set -g __fish_git_prompt_color_branch $_urdh_theme_vcs_color_branch
+    set -g __fish_git_prompt_color_merging $_urdh_theme_vcs_color_merging
+
+    set -ge __fish_git_prompt_showstashstate 
+    set -ge __fish_git_prompt_showdirtystate
+    set -ge __fish_git_prompt_showuntrackedfiles 
+    set -ge __fish_git_prompt_show_informative_status 
+    set -ge __fish_git_prompt_showupstream 
+
+    set -ge __fish_git_prompt_shorten_branch_len
+    set -ge __fish_git_prompt_shorten_branch_char_suffix
+
+    # Show the prompt!
     set -l ___vcs (vcprompt -f '%n')
     if test -n "$___vcs"
         switch $___vcs
-            case 'git*'
-                set -g __fish_git_prompt_color_branch 'black' '--bold'
-                set -g __fish_git_prompt_color_merging 'red' '--bold'
-                __fish_git_prompt ' %s'
-            case 'hg*'
-                printf ' %s%s%s' (set_color -o black) (vcprompt -f '%b') (set_color normal)
+            case 'git'
+                fish_git_prompt '%s'
+            case 'hg'
+                set -l ___branch_color (set_color $_urdh_theme_vcs_color_branch)
+                set -l ___normal_color (set_color normal)
+                set -l ___branch (vcprompt -f '%b')
+                printf '%s%s%s' $___branch_color $___branch $___normal_color
         end
     end
 end
@@ -34,6 +53,6 @@ function fish_prompt --description 'Write out the prompt'
     # print the prompt
     set -l ___host (printf '%s@%s' (_urdh_theme_user) $_urdh_theme_hostname)
     set -l ___cwd (_urdh_theme_cwd)
-    set -l ___vcs (printf '%s%s' (_urdh_theme_vcs))
+    set -l ___vcs (string join ' ' '' (_urdh_theme_vcs))
     printf '%s %s%s> ' $___host $___cwd $___vcs
 end
